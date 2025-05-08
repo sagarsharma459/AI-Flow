b693d6c06c5d7a78598fae2fcfefc8f1bd9a5874
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-05-08 18:05:13 +0300

    Delete apps/gateway/src/providers directory

--- apps/gateway/src/providers/github.ts
+++
@@ -1,6 +0,0 @@
-import { OAuth2Client } from 'google-auth-library';
-
-export async function githubOAuth(code: string) {
-  // TODO: exchange code for access_token, fetch profile
-  return { id: 'gh-id', email: 'user@github.com' };
-}
