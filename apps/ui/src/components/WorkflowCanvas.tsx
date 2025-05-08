7ee63c174dfa22104ff17dbe826b83da3e1f8ce8
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-05-08 18:20:27 +0300

    Create WorkflowCanvas.tsx

---
+++ apps/ui/src/components/WorkflowCanvas.tsx
@@ -0,0 +1,8 @@
+export default function WorkflowCanvas() {
+  return (
+    <div className="flex-1 bg-white border-l p-4">
+      {/* Canvas implementation (e.g. react-flow) */}
+      <p>Drag nodes here</p>
+    </div>
+  );
+}
